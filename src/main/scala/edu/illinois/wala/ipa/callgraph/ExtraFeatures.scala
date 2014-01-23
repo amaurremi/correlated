a0efe902080c730129ee5e4cb187971de11127ba
Marianna Rapoport <mrapoport@uwaterloo.ca>
2014-01-23 09:02:08 -0500

    Cosmetic changes

--- src/main/scala/edu/illinois/wala/ipa/callgraph/ExtraFeatures.scala
+++ src/main/scala/edu/illinois/wala/ipa/callgraph/ExtraFeatures.scala
@@ -1,4 +1,5 @@
 package edu.illinois.wala.ipa.callgraph
+
 import scala.collection.JavaConverters._
 import scala.collection._
 import edu.illinois.wala.Facade._
@@ -6,20 +7,22 @@ import edu.illinois.wala.S
 
 trait ExtraFeatures { self: AbstractCallGraphBuilder =>
 
-  lazy val allStatements = {
+  lazy val allStatements =
     (cg.asScala filter {
       _.getIR != null
     } map {
       n => n.instructions.map(i => S(n, i))
     }).flatten
-  }
 
   /**
    * find a call graph node that matches .*pattern.*
    */
   def findNode(pattern: String): Option[N] = {
     val p = ".*" + pattern + ".*"
-    cg.asScala.find(n => n.getMethod.toString.matches(p))
+    cg.asScala find {
+      n =>
+        n.getMethod.toString.matches(p)
+    }
   }
 
   /**
@@ -27,20 +30,29 @@ trait ExtraFeatures { self: AbstractCallGraphBuilder =>
    */
   def findNodes(pattern: String): Iterable[N] = {
     val p = ".*" + pattern + ".*"
-    cg.asScala.filter(n => n.getMethod.toString.matches(p))
+    cg.asScala filter {
+      n =>
+        n.getMethod.toString.matches(p)
+    }
   }
 
   /**
    * p.pt is the set of abstract objects pointed by p
    */
   implicit def pWithPointerObjects(p: P) = new {
-    def pt: Set[O] = (for (o <- heap.getSuccNodes(p).asScala) yield o.asInstanceOf[O]).toSet
+    def pt: Set[O] = (
+      for {
+        o <- heap.getSuccNodes(p).asScala
+      } yield o.asInstanceOf[O]
+    ).toSet
   }
 
   /**
    * localPtTo(o) is the local pointers to o
    */
-  def localPtTo(o: O): Iterable[P] = {
-    (for (p <- heap.getPredNodes(o).asScala if p.isInstanceOf[P]) yield p.asInstanceOf[P]).toIterable
-  }
+  def localPtTo(o: O): Iterable[P] = (
+    for {
+      p <- heap.getPredNodes(o).asScala if p.isInstanceOf[P]
+    } yield p.asInstanceOf[P]
+  ).toIterable
 }
