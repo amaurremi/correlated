05cba4c24ac2b8d12b40fa5667d03eae2cee79d6
Marianna Rapoport <mrapoport@uwaterloo.ca>
2014-01-22 21:56:52 -0500

    Reorganizing tests and removing redundant class files

--- src/test/scala/edu/illinois/wala/ipa/callgraph/FlexibleCallGraphBuilderTest.scala
+++ src/test/scala/edu/illinois/wala/ipa/callgraph/FlexibleCallGraphBuilderTest.scala
@@ -5,9 +5,9 @@ import com.typesafe.config.ConfigFactory
 import org.junit.Assert
 
 class FlexibleCallGraphBuilderTest {
-  @Test def testRegexEntrypoints {
+  @Test def testRegexEntrypoints() {
     val b = FlexibleCallGraphBuilder()(ConfigFactory.load().withFallback(
       ConfigFactory.parseString("wala.entry.signature-pattern = \".*createEntry\\\\(ILjava/lang/Object;Ljava/lang/Object;I\\\\)V.*\"")))
-    Assert.assertTrue(b.cg.getMaxNumber() > 20)
+    Assert.assertTrue(b.cg.getMaxNumber > 20)
   }
 }
