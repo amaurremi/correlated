0ce52cc27882367260820e5dba4e52f96a856d6b
Marianna Rapoport <mrapoport@uwaterloo.ca>
2014-01-28 18:09:09 -0500

    spacing

--- src/main/scala/ca/uwaterloo/correlated/CorrelatedCallsWriter.scala
+++ src/main/scala/ca/uwaterloo/correlated/CorrelatedCallsWriter.scala
@@ -11,38 +11,14 @@ object CorrelatedCallsWriter {
    * Traverses the call graph writing relevant information to CorrelatedCalls
    */
   def apply(
-             rcs: List[Set[CGNode]], cgNodes: List[CGNode]
-             ): CorrelatedCallWriter[_] = {
+    rcs: List[Set[CGNode]], cgNodes: List[CGNode]
+  ): CorrelatedCallWriter[_] = {
     import Scalaz._
 
     for {
       _ <- CorrelatedCalls(rcs = rcs).tell
       _ <- cgNodes.traverse[CorrelatedCallWriter, CGNode](cgNodeWriter(rcs.flatten.toSet))
     } yield ()
-  }
-
-  /**
-   * The implicit functions 's' and 'applicative' are necessary to use scalaz's Writer monad.
-   */
-  implicit val s = new Semigroup[CorrelatedCalls]{
-    def append(f1: CorrelatedCalls, f2: => CorrelatedCalls): CorrelatedCalls =
-      CorrelatedCalls(
-        f1.cgNodes ++ f2.cgNodes,
-        f1.rcs ++ f2.rcs,
-        f1.rcCcReceivers ++ f2.rcCcReceivers,
-        f1.receiverToCallSites ++ f2.receiverToCallSites,
-        f1.totalCallSites ++ f2.totalCallSites
-      )
-  }
-
-  implicit val applicative = new Applicative[CorrelatedCallWriter] {
-    def point[A](a: => A): CorrelatedCallWriter[A] = Writer(CorrelatedCalls.empty, a)
-
-    def ap[A, B](fa: => CorrelatedCallWriter[A])(f: => CorrelatedCallWriter[(A) => B]): CorrelatedCallWriter[B] =
-      for {
-        a  <- fa
-        f2 <- f
-      } yield f2(a)
   }
 
   /**
@@ -88,5 +64,29 @@ object CorrelatedCallsWriter {
 
   private[this] def callSiteIterator(cgNode: CGNode): Iterator[CallSiteReference] = {
     toScalaIterator(cgNode.iterateCallSites())
+  }
+
+  /**
+   * The implicit functions 's' and 'applicative' are necessary to use scalaz's Writer monad.
+   */
+  implicit val s = new Semigroup[CorrelatedCalls]{
+    def append(f1: CorrelatedCalls, f2: => CorrelatedCalls): CorrelatedCalls =
+      CorrelatedCalls(
+        f1.cgNodes ++ f2.cgNodes,
+        f1.rcs ++ f2.rcs,
+        f1.rcCcReceivers ++ f2.rcCcReceivers,
+        f1.receiverToCallSites ++ f2.receiverToCallSites,
+        f1.totalCallSites ++ f2.totalCallSites
+      )
+  }
+
+  implicit val applicative = new Applicative[CorrelatedCallWriter] {
+    def point[A](a: => A): CorrelatedCallWriter[A] = Writer(CorrelatedCalls.empty, a)
+
+    def ap[A, B](fa: => CorrelatedCallWriter[A])(f: => CorrelatedCallWriter[(A) => B]): CorrelatedCallWriter[B] =
+      for {
+        a  <- fa
+        f2 <- f
+      } yield f2(a)
   }
 }
