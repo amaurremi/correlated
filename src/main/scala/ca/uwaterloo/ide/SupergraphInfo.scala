a5fb352f1dc8c7e66fe323f6defd3287f3c2e650
mrapopor <mrapoport@uwaterloo.ca>
2014-02-14 13:46:20 -0500

    Doc

--- src/main/scala/ca/uwaterloo/ide/SupergraphInfo.scala
+++ src/main/scala/ca/uwaterloo/ide/SupergraphInfo.scala
@@ -4,12 +4,18 @@ import com.ibm.wala.dataflow.IFDS.ISupergraph
 
 class SupergraphInfo[T, P](superGraph: ISupergraph[T, P]) {
 
+  /**
+   * Returns all edges with a given source node
+   */
   def edgesWithSource(n: IdeNode[T]): Seq[IdeEdge[T]] = ???
 
+  /**
+   * Returns the edge function corresponding to an edge
+   */
   def edgeFn(edge: IdeEdge[T]): IdeFunction = ???
 
   /**
-   * Returns n's enclosing procedure
+   * Returns the enclosing procedure of a given node
    */
   def enclProc(n: T): T = ???
 
@@ -19,7 +25,8 @@ class SupergraphInfo[T, P](superGraph: ISupergraph[T, P]) {
   def startNode(node: IdeNode[T]): IdeNode[T] = ???
 
   /**
-   *
+   * Let p be the node's enclosing procedure. This method returns all
+   * edges from a p's caller node to the corresponding return node.
    */
   def callReturnEdges(node: IdeNode[T]): Seq[IdeEdge[T]] = {
     val proc = enclProc(node.n)
