6d00cae18a6bbc3fa3766903b3bb795e1cab8180
mrapopor <mrapoport@uwaterloo.ca>
2014-04-11 13:52:07 -0400

    implementing IFDS-to-IDE conversion

--- src/main/scala/ca/uwaterloo/id/ide/conversion/Ifds2Ide.scala
+++
@@ -1,9 +0,0 @@
-package ca.uwaterloo.id.ide.conversion
-
-import ca.uwaterloo.id.ide.IdeProblem
-import ca.uwaterloo.id.ifds.IfdsProblem
-
-object Ifds2Ide {
-
-  def convert(ifdsProblem: IfdsProblem): IdeProblem = ???
-}
