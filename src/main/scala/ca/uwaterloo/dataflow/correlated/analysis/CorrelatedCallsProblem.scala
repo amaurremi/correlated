163cb2ed20d9058c55d1b6c1a07d8f1d4613611b
mrapopor <mrapoport@uwaterloo.ca>
2014-04-15 13:56:11 -0400

    join of elems

--- src/main/scala/ca/uwaterloo/dataflow/correlated/analysis/CorrelatedCallsProblem.scala
+++ src/main/scala/ca/uwaterloo/dataflow/correlated/analysis/CorrelatedCallsProblem.scala
@@ -3,6 +3,7 @@ package ca.uwaterloo.dataflow.correlated.analysis
 import ca.uwaterloo.dataflow.ide.analysis.problem.IdeProblem
 import ca.uwaterloo.dataflow.ifds.analysis.problem.IfdsProblem
 import com.ibm.wala.classLoader.IMethod
+import scala.collection.breakOut
 
 trait CorrelatedCallsProblem extends IdeProblem { this: IfdsProblem =>
 
@@ -24,8 +25,16 @@ trait CorrelatedCallsProblem extends IdeProblem { this: IfdsProblem =>
   trait CorrelatedLatticeElem extends Lattice
 
   case class ReceiverToTypes(mapping: MultiMap) extends CorrelatedLatticeElem {
+    /**
+     * Defined as join. For two multi maps M1 and M2, computes a multi map M3
+     * that maps M1's and M2's keys to the union of their value sets.
+     */
     override def ⊓(el: ReceiverToTypes): ReceiverToTypes =
-      ???
+      ReceiverToTypes(((mapping.keySet ++ el.mapping.keySet) map {
+        key =>
+          val getTypes = (m: ReceiverToTypes) => m.mapping get key getOrElse Set.empty
+          key -> (getTypes(this) ++ getTypes(el))
+      })(breakOut))
   }
 
   trait CorrelatedFunction extends IdeFunctionI
