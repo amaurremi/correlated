d99e975888fba9a162ba7d616a70d53bad6a3f36
mrapopor <mrapoport@uwaterloo.ca>
2014-04-15 13:07:13 -0400

    renaming

--- src/test/scala/ca/uwaterloo/dataflow/ide/taint/TaintAnalysisSpec.scala
+++ src/test/scala/ca/uwaterloo/dataflow/ide/taint/TaintAnalysisSpec.scala
@@ -10,15 +10,15 @@ class TaintAnalysisSpec extends FunSpec {
 
    describe("TaintAnalysis") {
      it("propagates secret values intra-procedurally") {
-       val ifds = new TaintAnalysisSpecBuilder("LocalVars")
-       import ifds._
+       val taint = new TaintAnalysisSpecBuilder("LocalVars")
+       import taint._
 
        variable("x", "main") shouldBe secret
      }
 
      it("propagates non-secret values intra-procedurally") {
-       val ccs = new TaintAnalysisSpecBuilder("NotSecretLocalVars")
-       import ccs._
+       val taint = new TaintAnalysisSpecBuilder("NotSecretLocalVars")
+       import taint._
 
        variable("x", "main") shouldBe notSecret
        variable("y", "main") shouldBe notSecret
@@ -26,43 +26,43 @@ class TaintAnalysisSpec extends FunSpec {
      }
 
      it("propagates secret values along the call-start edge") {
-       val ccs = new TaintAnalysisSpecBuilder("FunctionCall")
-       import ccs._
+       val taint = new TaintAnalysisSpecBuilder("FunctionCall")
+       import taint._
 
        variable("s", "f") shouldBe secret
      }
 
      it("sets a function parameter to top, if that function is invoked with secret and non-secret arguments") {
-       val ccs = new TaintAnalysisSpecBuilder("MultipleFunctionCalls")
-       import ccs._
+       val taint = new TaintAnalysisSpecBuilder("MultipleFunctionCalls")
+       import taint._
 
        variable("s", "f") shouldBe secret
      }
 
      it("propagates secret values along the end-return edge") {
-       val ccs = new TaintAnalysisSpecBuilder("ReturnSecret")
-       import ccs._
+       val taint = new TaintAnalysisSpecBuilder("ReturnSecret")
+       import taint._
 
        variable("s", "main") shouldBe secret
      }
 
      it("propagates secret-value-storing variables along the end-return edge") {
-       val ccs = new TaintAnalysisSpecBuilder("Return")
-       import ccs._
+       val taint = new TaintAnalysisSpecBuilder("Return")
+       import taint._
 
        variable("s", "main") shouldBe secret
      }
 
      it("assigns top to variables that have been assigned secret and non-secret values in if branches") {
-       val ccs = new TaintAnalysisSpecBuilder("Phi")
-       import ccs._
+       val taint = new TaintAnalysisSpecBuilder("Phi")
+       import taint._
 
        variable("s", "main") shouldBe secret
      }
 
      it("propagate constant variables that have been assigned the same value in different if branches") {
-       val ccs = new TaintAnalysisSpecBuilder("PhiSame")
-       import ccs._
+       val taint = new TaintAnalysisSpecBuilder("PhiSame")
+       import taint._
 
        variable("s", "main") shouldBe secret
      }
