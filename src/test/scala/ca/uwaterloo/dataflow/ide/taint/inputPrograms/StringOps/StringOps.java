7c7fea9a3a0be58349703136d7591f7b085a8692
mrapopor <mrapoport@uwaterloo.ca>
2014-04-23 15:12:14 -0400

    minimal example

--- src/test/scala/ca/uwaterloo/dataflow/ide/taint/inputPrograms/StringOps/StringOps.java
+++ src/test/scala/ca/uwaterloo/dataflow/ide/taint/inputPrograms/StringOps/StringOps.java
@@ -5,16 +5,17 @@ import ca.uwaterloo.dataflow.ide.taint.inputPrograms.SecretAssertions;
 public class StringOps {
 
     public static void main(String[] args) {
-        String s = secret();
-        SecretAssertions.shouldBeSecret(s);
-        String s2 = s.toLowerCase();
-        String s3 = s.substring(0);
-        String s4 = s.trim();
-        String s5 = s.toUpperCase();
-        SecretAssertions.shouldBeSecret(s2);
-        SecretAssertions.shouldBeSecret(s3);
-        SecretAssertions.shouldBeSecret(s4);
-        SecretAssertions.shouldBeSecret(s5);
+//        String s = secret();
+//        SecretAssertions.shouldBeSecret(s);
+        String s2 = "".trim();
+        SecretAssertions.shouldNotBeSecret(s2);
+//        String s3 = s.substring(0);
+//        String s4 = s.trim();
+//        String s5 = s.toUpperCase();
+//        SecretAssertions.shouldBeSecret(s2);
+//        SecretAssertions.shouldBeSecret(s3);
+//        SecretAssertions.shouldBeSecret(s4);
+//        SecretAssertions.shouldBeSecret(s5);
     }
 
     static String secret() {
