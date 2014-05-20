13bc51ce04cdf01991f7eb29ccef9c73f9969c01
mrapopor <mrapoport@uwaterloo.ca>
2014-05-20 11:58:56 -0400

    modifying test

--- src/test/scala/ca/uwaterloo/dataflow/ide/taint/inputPrograms/StringBuilder1/StringBuilder1.java
+++ src/test/scala/ca/uwaterloo/dataflow/ide/taint/inputPrograms/StringBuilder1/StringBuilder1.java
@@ -5,10 +5,13 @@ import ca.uwaterloo.dataflow.ide.taint.inputPrograms.SecretAssertions;
 public class StringBuilder1 {
 
     StringBuilder sb;
+    static StringBuilder sb2;
 
     public static void main(String[] args) {
         StringBuilder1 sb1 = new StringBuilder1();
         sb1.sb = new StringBuilder("not secret");
         SecretAssertions.secret(sb1.sb);
+        sb2 = new StringBuilder();
+        SecretAssertions.secret(sb2);
     }
 }
