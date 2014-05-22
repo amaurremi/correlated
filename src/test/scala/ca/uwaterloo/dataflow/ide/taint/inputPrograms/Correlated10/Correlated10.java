eb6ed336d8eedb0ecf537aac717bf25a3869da9c
mrapopor <mrapoport@uwaterloo.ca>
2014-05-22 15:26:26 -0400

    reformatting

--- src/test/scala/ca/uwaterloo/dataflow/ide/taint/inputPrograms/Correlated10/Correlated10.java
+++ src/test/scala/ca/uwaterloo/dataflow/ide/taint/inputPrograms/Correlated10/Correlated10.java
@@ -3,22 +3,30 @@ package ca.uwaterloo.dataflow.ide.taint.inputPrograms.Correlated10;
 import ca.uwaterloo.dataflow.ide.taint.inputPrograms.SecretAssertions;
 
 class A {
-  public String foo(){ return Correlated10.secret(); }
-  public void bar(String s){ }
+    public String foo() {
+        return Correlated10.secret();
+    }
+
+    public void bar(String s) {
+    }
 }
+
 class B extends A {
-  public String foo(){ return null; }
-  public void bar(String s){
-       SecretAssertions.notSecret(s);
-  }
+    public String foo() {
+        return null;
+    }
+
+    public void bar(String s) {
+        SecretAssertions.notSecret(s);
+    }
 }
 
 public class Correlated10 {
 
     public static void main(String[] args) {
-       A x = (args.length == 2) ? new A() : new B();
-       String v = x.foo();
-       x.bar(v);
+        A x = (args.length == 2) ? new A() : new B();
+        String v = x.foo();
+        x.bar(v);
     }
 
     static String secret() {
