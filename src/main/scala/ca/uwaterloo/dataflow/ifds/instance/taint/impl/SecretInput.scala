0cf2ecdc829c43a140f7eb491eb86ca2cfe36a64
mrapopor <mrapoport@uwaterloo.ca>
2014-05-28 17:31:08 -0400

    refactoring

--- src/main/scala/ca/uwaterloo/dataflow/ifds/instance/taint/impl/SecretInput.scala
+++ src/main/scala/ca/uwaterloo/dataflow/ifds/instance/taint/impl/SecretInput.scala
@@ -1,20 +1,7 @@
 package ca.uwaterloo.dataflow.ifds.instance.taint.impl
 
-import ca.uwaterloo.dataflow.ifds.instance.taint.SecretDefinition
-import com.ibm.wala.analysis.typeInference.TypeAbstraction
-import com.ibm.wala.classLoader.IMethod
-import com.ibm.wala.ipa.callgraph.CGNode
-import com.ibm.wala.types.{MethodReference, TypeReference}
 
-trait SecretInput extends SecretDefinition {
+trait SecretInput extends SecretDefFromConfig {
 
-  override def getOperationType(op: MethodReference, node: CGNode, vn: Option[ValueNumber]): Option[SecretOperation] = ???
-
-  override def isSecretArrayElementType(typeRef: TypeReference): Boolean = ???
-
-  override def isConcatClass(typeRef: TypeAbstraction): Boolean = ???
-
-  override def secretTypes: Set[String] = ???
-
-  override def isSecret(method: IMethod): Boolean = ???
+  override def configPath = "src/main/scala/ca/uwaterloo/dataflow/ifds/instance/taint/impl/resources/SecretInput.conf"
 }
