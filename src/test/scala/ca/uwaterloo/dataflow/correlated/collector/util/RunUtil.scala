ad72dbb13be482da16651d01b0ef6f355e71e349
Marianna Rapoport <mrapoport@uwaterloo.ca>
2014-12-02 13:00:05 -0500

    unnecessary new line

--- src/test/scala/ca/uwaterloo/dataflow/correlated/collector/util/RunUtil.scala
+++ src/test/scala/ca/uwaterloo/dataflow/correlated/collector/util/RunUtil.scala
@@ -44,7 +44,7 @@ trait RunUtil {
       file =>
         val name = file.getName
         if (name endsWith "jar") {
-          println(s"Running $name benchmark...\n")
+          println(s"Running $name benchmark...")
           runner(bmCollectionName, name.substring(0, name.lastIndexOf('.')))
         }
     }
