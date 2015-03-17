508f8e65702df32fceec40c94c1034928f1c5cfa
Marianna Rapoport <mrapoport@uwaterloo.ca>
2015-03-17 10:17:35 -0400

    minor changes

--- src/main/scala/ca/uwaterloo/dataflow/ifds/instance/taint/impl/CcReceivers.scala
+++ src/main/scala/ca/uwaterloo/dataflow/ifds/instance/taint/impl/CcReceivers.scala
@@ -1,10 +1,12 @@
 package ca.uwaterloo.dataflow.ifds.instance.taint.impl
 
-import ca.uwaterloo.dataflow.common.SuperGraphTypes
+import ca.uwaterloo.dataflow.common.{Time, SuperGraphTypes}
 import ca.uwaterloo.dataflow.correlated.analysis.Receivers
 import ca.uwaterloo.dataflow.correlated.collector.{CorrelatedCallStats, ReceiverI}
 
 trait CcReceivers extends Receivers { this: SuperGraphTypes =>
 
-  override def getCcReceivers: Set[ReceiverI] = CorrelatedCallStats(callGraph).receiverToCallSites.keys.toSet
+  override def getCcReceivers: Set[ReceiverI] = Time.time ("  Collecting receivers...") {
+    CorrelatedCallStats(callGraph).receiverToCallSites.keys.toSet
+  }
 }
